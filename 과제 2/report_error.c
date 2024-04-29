d6f45382d8193378c63f4390bbd64f367794ce9e
지현서 <113419425+3veryDay@users.noreply.github.com>
2024-04-29 12:11:46 +0900

    Update report_error.c

--- 과제 2/report_error.c
+++ 과제 2/report_error.c
@@ -28,12 +28,12 @@ void report_error(int i, char *s) {
     //Illegal Identifier - over 12 characters
     //식별자는 12자 이내이어야 함
     if (strlen(s) >= 12) {
-        printf("%s\t-> Too Long Identifier, over 12 characters\n", s);
+        printf("%s\t-> Too Long Identifier, Over 12 characters\n", s);
     }
     //Illegal Identifier - Starts with Digit
     //식별자는 숫자로 시작하면 안됨
     else if(isdigit(s[0])) {
-        printf("%s\t\t-> Starts with Digit\n",s);
+        printf("%s\t\t-> Illegal Identifier, Starts with Digit\n",s);
     }
     //Illegal Character
     //정의되지 않은 문자를 사용하면 안됨
