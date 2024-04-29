829ecbf43dd94fb142413e3b7c1faa3f6c899476
sohvun <113416590+sohvun@users.noreply.github.com>
2024-04-29 16:01:52 +0900

    Update symtable.c

--- 과제 2/symtable.c
+++ 과제 2/symtable.c
@@ -11,14 +11,15 @@
 #define STsize 1000 // String Table 사이즈
 
 char ST[STsize];   // String Table 정의
-int STindex = 0;   // 다음으로 사용 가능한 ST의 인덱스
+int STindextail = 0;   // 다음으로 사용 가능한 ST의 인덱스
+int STindex = 0;
 int tmp = 0;
 
 // ST에 동일한 identifier가 있는지 확인하는 함수
 int isIdentifierExists(char *yytext) {
     int i, j;
     int yytext_len = strlen(yytext);
-    for (i = 0; i < STindex; i++) {
+    for (i = 0; i < STindextail; i++) {
         // Compare each character of yytext with the characters in ST
         for (j = 0; j < yytext_len; j++) {
             if (ST[i + j] != yytext[j]) {
@@ -36,15 +37,15 @@ int isIdentifierExists(char *yytext) {
 
 // 기존의 것과 겹치지 않는 new identifier일 때 실행
 void symtable(char *yytext) {
-    if (tmp != 0) { STindex = tmp; tmp = 0; }
+    if (tmp != 0) { STindextail = tmp; tmp = 0; }
     // 존재 여부 확인
     int exist = isIdentifierExists(yytext);
     if (exist == 0) {
         int i = 0;
         while (yytext[i] != '\0') {
             // ST에 식별자의 각 문자 추가
-            if (STindex < STsize) {
-                ST[STindex++] = yytext[i];
+            if (STindextail < STsize) {
+                ST[STindextail++] = yytext[i];
             } else {
                 printf("Error: String Table overflow\n");
                 exit(EXIT_FAILURE); 
@@ -52,15 +53,17 @@ void symtable(char *yytext) {
             i++;
         }
         // 식별자의 끝을 나타내는 널 문자 추가
-        if (STindex < STsize) {
-            ST[STindex++] = '\0';
+        if (STindextail < STsize) {
+            ST[STindextail++] = '\0';
+            STindex = STindextail - (strlen(yytext) + 1);
         } else {
             printf("Error: String Table overflow\n");
             exit(EXIT_FAILURE);
         }
     }
     else {
-        tmp = STindex;
-        STindex = exist + 1;
+        tmp = STindextail;
+        STindextail = exist + 1;
+        STindex = STindextail - (strlen(yytext) + 1);
     }
 }
