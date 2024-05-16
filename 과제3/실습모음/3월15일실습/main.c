ca2f67e0d2f13db814f505919732359f8c630b89
지현서 <113419425+3veryDay@users.noreply.github.com>
2024-05-16 16:16:55 +0900

    Update main.c

--- 과제3/실습모음/3월15일실습/main.c
+++ 과제3/실습모음/3월15일실습/main.c
@@ -3,5 +3,12 @@ extern int yyparse();
 
 int main()
 {
-  return yyparse();
+
+
+    int parse_result = yyparse();
+
+    printf("2171047 JoYoona\n2176143 ParkSohyun\n2176365 JoEunhye\n2176368 JiHyunseo\n");
+
+    return parse_result;
+
 }
