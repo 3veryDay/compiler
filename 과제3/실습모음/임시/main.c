19d5d298c6903070499532dfb52877acc012b8f3
eunhye <113422802+eunhye515@users.noreply.github.com>
2024-05-20 15:23:17 +0900

    Update main.c

--- 과제3/실습모음/임시/main.c
+++ 과제3/실습모음/임시/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 extern int yyparse();
 void main() {
-  printf("start of parser\n");
-  yyparse();
-  printf("end of parser\n");
+  return yyparse();
   printf("2171047 JoYoona\n2176143 ParkSohyun\n2176365 JoEunhye\n2176368 JiHyunseo\n");
 }
