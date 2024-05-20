26da52edeb523b79e6acc75ed2129cc12b96dc8f
sohvun <113416590+sohvun@users.noreply.github.com>
2024-05-20 22:50:54 +0900

    Update main.c

--- 과제3/실습모음/임시/main.c
+++ 과제3/실습모음/임시/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include "tn.h"
 extern int yyparse();
 
 int main() {
 
   int parse_result = yyparse();
-  return parse_result;
+  
   printf("2171047 JoYoona\n2176143 ParkSohyun\n2176365 JoEunhye\n2176368 JiHyunseo\n");
+
+  return parse_result;
 }
