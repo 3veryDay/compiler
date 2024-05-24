a6965f72c296e6f311cb33a5c4086284c0d6a3b6
idknow417 <101107092+idknow417@users.noreply.github.com>
2024-05-25 00:18:58 +0900

    Update ReportError.c

--- 과제3/과제3/ReportError.c
+++ 과제3/과제3/ReportError.c
@@ -1,3 +1,9 @@
-void ReportError(int i, ErrorType err) //error line, error type
+#include <stdio.h>
+#include <stdlib.h>
+#include "glob.h"
+// 라인 넘버 변수는 lineno , glob.h에 정의되어 있음
+// ErrorType 도 glob.h에 정의
+
+void ReportError(ErrorType err)
 {
 }
