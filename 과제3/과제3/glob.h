7a5beb64092a61d4d2c154e0496fc5e9fa90c6b7
idknow417 <101107092+idknow417@users.noreply.github.com>
2024-05-24 23:23:14 +0900

    Update glob.h

--- 과제3/과제3/glob.h
+++ 과제3/과제3/glob.h
@@ -1,4 +1,4 @@
-enum errorType { noerror, illid_digit, illid_long, illid_illch, illid_illegal, overst, missing_semi, missing_sbracket, missing_mbracket, missing_lbracket, missing_comma, missing_funcheader, declaring_err, missing_condition, wrong_assign, missing_operand
+enum errorType { noerror, digit_ident, long_ident, ill_symbol, over_st, miss_semi, miss_paren, miss_curly, miss_square, miss_comma, miss_funcheader, declaring_err, miss_condition, wrong_assign, miss_operand
 };
 typedef enum errorType ErrorType;
 ErrorType err;
