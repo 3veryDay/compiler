23926e4d8d21b878c21ef209f5a3518e952adb12
idknow417 <101107092+idknow417@users.noreply.github.com>
2024-05-25 00:43:50 +0900

    Update glob.h

--- 과제3/과제3/glob.h
+++ 과제3/과제3/glob.h
@@ -1,6 +1,41 @@
+/*
+ * glob.h - global variable for the project
+ */
+#define STsize 1000
+#define HTsize 100
+#define FALSE 0
+#define TRUE 1
+
+typedef struct HTentry* HTpointer;
+typedef struct HTentry {
+	int index;
+	int isConst;
+	int isFunction;
+	int isParam;
+	int isArray;
+	int linenum;
+	int error;
+	Types spec;
+	HTpointer next;
+} HTentry;
+
+HTpointer HT[HTsize];
+char ST[STsize];
+HTpointer current_id;
+
+int nextid; 
+int nextfree;
+int hashcode; 
+int sameid;  //st, ht용
+
+int cErrors;  //number of errors
+int lineno;
+
 enum errorType { noerror, digit_ident, long_ident, ill_symbol, over_st, missing_semi, missing_paren, missing_curly, missing_square, missing_comma, missing_funcheader, declaring_err, missing_condition, wrong_assign, missing_operand
 };
 typedef enum errorType ErrorType;
 ErrorType err;
 
-int lineno;
+enum Types { INT, VOID, FLOAT, NONE};
+typedef enum Types Types;
+Types type;
