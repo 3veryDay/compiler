3965c146e9dfdd0590600dc22065d4d0a5150c20
idknow417 <101107092+idknow417@users.noreply.github.com>
2024-05-24 23:16:30 +0900

    Create glob.h

---
+++ 과제3/과제3/glob.h
@@ -0,0 +1,4 @@
+enum errorType { noerror, illid_digit, illid_long, illid_illch, illid_illegal, overst, missing_semi, missing_sbracket, missing_mbracket, missing_lbracket, missing_comma, missing_funcheader, declaring_err, missing_condition, wrong_assign, missing_operand
+};
+typedef enum errorType ErrorType;
+ErrorType err;
