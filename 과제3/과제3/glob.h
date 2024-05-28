ba6c40d6261a95f7a2102d4f60f8e9b15e2a5042
idknow417 <101107092+idknow417@users.noreply.github.com>
2024-05-28 22:01:34 +0900

    Update glob.h

--- 과제3/과제3/glob.h
+++ 과제3/과제3/glob.h
@@ -6,7 +6,7 @@
 #define FALSE 0
 #define TRUE 1
 
-enum Types { INT, VOID, FLOAT, NONE};
+enum Types { INT, VOID, FLOAT, NONE };
 typedef enum Types Types;
 Types type;
 
@@ -27,18 +27,19 @@ HTpointer HT[HTsize];
 char ST[STsize];
 HTpointer current_id;
 
-int nextid; 
+int nextid;
 int nextfree;
-int hashcode; 
+int hashcode;
 int sameid;  //st, ht용
-int found;
 
 int cErrors;  //number of errors
+int found;
 int lineno;
 
-enum ErrorType { noerror, digit_ident, long_ident, ill_symbol, over_st, missing_semi, 
-		missing_lparen, missing_lcurly, missing_lsquare, missing_rparen, missing_rcurly, missing_rsquare, 
-		missing_comma, missing_funcheader, declaring_err, missing_condition, wrong_assign, missing_operand
+enum ErrorType {
+	noerror, digit_ident, long_ident, ill_symbol, over_st, missing_semi,
+	missing_lparen, missing_lcurly, missing_lsquare, missing_rparen, missing_rcurly, missing_rsquare,
+	missing_comma, missing_funcheader, declaring_err, missing_condition, wrong_assign, missing_operand
 };
 typedef enum ErrorType ErrorType;
 ErrorType err;
