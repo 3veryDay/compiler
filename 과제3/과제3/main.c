461c1769b9c86f2b48378b71fcc07a62a508a6eb
sohvun <113416590+sohvun@users.noreply.github.com>
2024-05-28 15:45:31 +0900

    Create main.c

---
+++ 과제3/과제3/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "tn.h" 
+#include "glob.h" 
+
+extern yylex();
+extern char *yytext;
+void printHSTable();
+
+void main()
+{
+    cLine = 1;
+	printf("******************************[[MiniC parsing begins]]\n");
+	yyparse();
+	printf("[[Parsing Ends]]**************************************\n");
+	printf("\n\n%d error(s) detected\n\n", cErrors);
+	printf("====================[[Hash Table]]====================\n");
+
+	printHT();
+}
+
+
+void printHT(){
+	HTpointer here;
+	int i, j;
+
+	for (i = 0; i < HTsize; i++) {
+		if (HT[i] != NULL) {
+			here = HT[i];			
+			while (here != NULL) {
+				printf("HashCode %d:	 ", i);
+				j = here->index;
+				while (ST[j] != '\0') {
+					printf("%c", ST[j++]);
+				}
+				if (here->spec == NONE) {
+					printf(" / not defined ");
+				}
+				else if (!here->error) {
+					if (here->isFunction) { 
+						switch (here->spec)
+						{
+						case  INT:
+							printf(" / Return Type: Integer ");
+							break;
+						case  VOID:
+							printf(" / Return Type: Void ");
+							break;
+						case  FLOAT:
+							printf(" / Return Type: Float ");
+							break;
+						}
+						printf("Function ");
+					}
+					else if (!here->isFunction) { 
+						if (here->isConst) {
+							printf(" / Const ");
+						}
+						switch (here->spec)
+						{
+						case  INT:
+							printf(" / Integer ");
+							break;
+						case  FLOAT:
+							printf(" / Float ");
+							break;
+						}
+						if (here->isArray) { // Array
+							printf("Array ");
+						}
+						else {
+							printf("Scalar ");
+						}
+						if (here->isParam) {
+							printf("Parameter ");
+						}
+
+						printf("Variable ");
+
+				}
+				
+				}
+				else if(here->error) {
+					printf(" / not defined ");
+				}
+				printf("/ Linenum: %d \n", here->linenum);
+				here = here->next;
+			}
+		}
+	}
+	
+}
