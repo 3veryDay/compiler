3f3734b93f0ed31cb5fd8ef8ca3d8784ed777cdc
sohvun <113416590+sohvun@users.noreply.github.com>
2024-05-28 14:11:40 +0900

    Update ReportError.c

--- 과제3/과제3/ReportError.c
+++ 과제3/과제3/ReportError.c
@@ -6,4 +6,72 @@
 
 void ReportError(ErrorType err)
 {
+	switch(err)
+	{
+	case noerror: 
+		break;
+  case digit_ident:
+		cErrors++;
+		printf("%6d          ERROR!!			  %-10s		start with digit\n", lineno, yytext);
+		break;
+	case long_ident:
+		cErrors++;
+		printf("%6d          ERROR!!			  %-13s	 too long identifier\n", lineno, yytext);
+		break;
+	case ill_symbol:
+		cErrors++;
+		printf("%6d          ERROR!!			  %-5c		illegal symbol\n", lineno, yytext[0]);
+		break;
+	/* case illid_illegal:
+		cErrors++;
+		printf("%6d          ERROR!!			  %-5s		illegal identifier\n", lineno, yytext);
+		break; */
+	case over_st:
+		cErrors++;
+		printf("\nError : OVERFLOW in ST\n");
+		printf("==================================================\n");
+		exit(0);
+		break;
+	case missing_semi:
+		cErrors++;
+		printf("%6d          ERROR!!			  missing semicolon\n", lineno-1);
+		break;
+	case missing_paren:
+		cErrors++;
+		printf("%6d          ERROR!!			  not closed parenthesis\n", lineno);
+		break;
+	case missing_curly:
+		cErrors++;
+		printf("%6d          ERROR!!			  not closed curly bracket\n", lineno);
+		break;
+	case missing_square:
+		cErrors++;
+		printf("%6d          ERROR!!			  not closed square bracket\n", lineno);
+		break;
+	case missing_comma:
+		cErrors++;
+		printf("%6d          ERROR!!			  missing comma\n", lineno);
+		break;
+	case missing_funcheader:
+		cErrors++;
+		printf("%6d          ERROR!!			  no function header\n", lineno);
+		break;
+	case declaring_err:
+		cErrors++;
+		printf("%6d          ERROR!!              wrong declaration\n", lineno);
+		break;
+	case missing_condition:
+		cErrors++;
+		printf("%6d          ERROR!!              missing condition\n", lineno);
+		break;
+	case wrong_assign:
+		cErrors++;
+		printf("%6d          ERROR!!              wrong assignment\n", lineno);
+		break;
+	case missing_operand:
+		cErrors++;
+		printf("%6d          ERROR!!              missing operand\n", lineno);
+		break;
+		
+	}
 }
